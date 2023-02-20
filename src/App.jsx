eb6f9d0405259997cb3b3e6469d78bae9475e500
JavierTorres1193 <javier.torres1193@gmail.com>
2023-02-20 18:04:18 -0300

    push

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Principal } from './Componentes/Principal'
+import { Link,Route, Routes } from 'react-router-dom'
+import { ListaRemeras } from './componentes/remeras'
+import { ListaPantalones } from './Componentes/pantalones'
+import { ListaChanclas } from './Componentes/chanclas'
+import { ListaBuzos } from './Componentes/buzos'
+import { ListaMallas } from './Componentes/mallas'
+
+function App() {
+  return (
+    <>
+    
+    <div className="App">
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand">Banzai Clothes</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+      <Link className="nav-link" to={'/'}>Inicio</Link>
+      </li>
+
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Productos
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <Link className="dropdown-item" to={'/listarbuzos'}>Buzos y camperas</Link>
+          <Link className="dropdown-item" to={'/listarchanclas'}>Chanclas</Link>
+          <Link className="dropdown-item" to={'/listarmallas'}>Mallas</Link>
+          <Link className="dropdown-item" to={'/listarpantalones'}>Pantalones</Link>
+          <Link className="dropdown-item" to={'/listarremeras'}>Remeras</Link>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+  
+        <Routes>
+          <Route path='/' element={<Principal/>}></Route> 
+          <Route path='/listarremeras' element={<ListaRemeras/>}></Route>
+          <Route path='/listarpantalones' element={<ListaPantalones/>}></Route>
+          <Route path='listarchanclas' element={<ListaChanclas/>}></Route>
+          <Route path='listarbuzos' element={<ListaBuzos/>}></Route>
+          <Route path='listarmallas' element={<ListaMallas/>}></Route>
+
+        </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/mallas.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export function ListaMallas(){
+    return(
+
+        <div class="card">
+        <div class="card-header">
+          Stock de Mallas
+        </div>
+        <div class="card-body">
+        <div class="table-responsive">
+                <table class="table table-primary">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Talle</th>
+                            <th scope="col">Cantidad</th>
+                            <th scope="col">Color</th>
+                            <th scope="col">Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="">
+                            <td scope="row">R1C1</td>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+
+                        </tr>
+                        <tr class="">
+                            <td scope="row">Item</td>
+                            <td>Item</td>
+                            <td>Item</td>
+                            <td>Item</td>
+                            <td>Item</td>
+
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+      </div>
+            
+            
+
+    )  
+}
