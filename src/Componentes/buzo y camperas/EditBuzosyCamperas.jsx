feb8526f4f1f07e66c9dd14bd3279aea7d4bc0ed
JavierTorres1193 <javier.torres1193@gmail.com>
2023-03-10 00:46:22 -0300

    Update EditBuzosyCamperas.jsx

--- src/Componentes/buzo y camperas/EditBuzosyCamperas.jsx
+++ src/Componentes/buzo y camperas/EditBuzosyCamperas.jsx
@@ -22,6 +22,7 @@ export function EditBuzosyCamperas(){
 
 
     const trae_datos = async ()=>{
+        const datos= await API.getBuzosyCamperasById(idBuzosyCamperas);
         console.log('los datos enviados son',datos[0].Talle)
             setTalle(datos[0].Talle)
             setCantidad(datos[0].Cantidad)
